482f5f7c86c866614b9ef2c59036db2a65248767
Admin <wangbaicheng142@gmail.com>
2025-09-30 21:47:20 +0800

    数据写入方法：食物，摄取

--- core/data/dataUtil.py
+++ core/data/dataUtil.py
@@ -6,18 +6,19 @@ from core.models.food import Food
 # 添加新食物
 def add_food(food):
     with open(file='food.csv',mode='a',encoding='utf-8') as f:
-        f.write(food.to_str())
+        f.write(food.to_str()+'\n')
     f.close()
 
 # 添加进食记录
 def add_intake(food,n):
     date=datetime.datetime.now().date()
-    with open(file='intake.csv',mode='a',encoding='utf-8') as f:
-        f.write()
+    with open(file='food_intake.csv',mode='a',encoding='utf-8') as f:
+        f.write(food.to_str()+','+str(n)+','+str(date)+'\n')
+    f.close()
 
 
 
 if __name__ == '__main__':
-    # food=Food('鸡腿','0','20','10')
-    # add_food(food)
-    add_intake(123,1)
+    food=Food('鸡腿','0','20','10')
+
+    add_intake(food,1)
